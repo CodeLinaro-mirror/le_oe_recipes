mcharleb/meta-qti-bsp

OE framework update 2/15
Explicitly depend on pkgconfig when used.
Removed avahi, dpkg, lighttpd to use updated versions from meta.
Removed unused configure options.
Removed unfound license warning.
FILESEXTRAPATHS syntax updated.
ROOTFS_POSTINSTALL_COMMAND syntax updated.
IMAGE_PREPROCESS_COMMAND syntax updated.
IMAGE_POSTPROCESS_COMMAND syntax updated.
Deprecated build targets removed.
Clean up conf/distro/msm.conf.
PRs of changed recipes are bumped.

Change-Id: Ifc199a24b977a5791aae38ef771325045ceaa34a

recipes/iptables/iptables_1.4.12.1.bb
@@ -3,12 +3,13 @@ DESCRIPTION = "iptables is the userspace command line program used to configure 
  and control network packet filtering code in Linux."
 HOMEPAGE = "http://www.netfilter.org/"
 BUGTRACKER = "http://bugzilla.netfilter.org/"
-DEPENDS = "virtual/kernel"
 LICENSE = "GPLv2+"
 LIC_FILES_CHKSUM = "file://COPYING;md5=b234ee4d69f5fce4486a80fdaf4a4263\
                     file://iptables/iptables.c;firstline=13;endline=25;md5=f1028f2401da1c120df27594b2be50ef"
 
 PR = "r3"
+DEPENDS        = "virtual/kernel"
+RDEPENDS_${PN} = "libnfnetlink"
 
 RRECOMMENDS_${PN} = "kernel-module-x-tables \
                      kernel-module-ip-tables \
@@ -19,8 +20,9 @@ RRECOMMENDS_${PN} = "kernel-module-x-tables \
                      kernel-module-nf-conntrack-ipv4 \
                      kernel-module-nf-nat \
                      kernel-module-ipt-masquerade"
-FILES_${PN} =+ "${libdir}/xtables/"
+FILES_${PN}     =+ "${libdir}/xtables/"
 FILES_${PN}-dbg =+ "${libdir}/xtables/.debug"
+FILES_${PN}-doc =+ "${datadir}/xtables/"
 
 # The types patch is in the upstream folder, add it to the filespath
 FILESPATH =+ "${COREBASE}/meta/recipes-extended/${PN}/${PN}:"
@@ -34,9 +36,9 @@ SRC_URI[md5sum] = "b08a1195ec2c1ebeaf072db3c55fdf43"
 SRC_URI[sha256sum] = "77e6581f21f15946a814fa311236e5f3f7c6593180f9d695cea06aa95e464aba"
 
 
-CFLAGS += "-I${STAGING_KERNEL_DIR}/source/include/linux/netfilter_ipv4"
+CFLAGS += "-I${STAGING_KERNEL_DIR}/usr/include/linux/netfilter_ipv4"
 
-inherit autotools
+inherit autotools pkgconfig
 
 EXTRA_OECONF = "--with-kernel=${STAGING_INCDIR} \
                 ${@base_contains('DISTRO_FEATURES', 'ipv6', '', '--disable-ipv6', d)}"

CodeLinaro-mirror/le_oe_recipes

recipes/iptables/iptables_1.4.12.1.bb
@@ -3,12 +3,13 @@ DESCRIPTION = "iptables is the userspace command line program used to configure 
  and control network packet filtering code in Linux."
 HOMEPAGE = "http://www.netfilter.org/"
 BUGTRACKER = "http://bugzilla.netfilter.org/"
-DEPENDS = "virtual/kernel"
 LICENSE = "GPLv2+"
 LIC_FILES_CHKSUM = "file://COPYING;md5=b234ee4d69f5fce4486a80fdaf4a4263\
                     file://iptables/iptables.c;firstline=13;endline=25;md5=f1028f2401da1c120df27594b2be50ef"
 
 PR = "r3"
+DEPENDS        = "virtual/kernel"
+RDEPENDS_${PN} = "libnfnetlink"
 
 RRECOMMENDS_${PN} = "kernel-module-x-tables \
                      kernel-module-ip-tables \
@@ -19,8 +20,9 @@ RRECOMMENDS_${PN} = "kernel-module-x-tables \
                      kernel-module-nf-conntrack-ipv4 \
                      kernel-module-nf-nat \
                      kernel-module-ipt-masquerade"
-FILES_${PN} =+ "${libdir}/xtables/"
+FILES_${PN}     =+ "${libdir}/xtables/"
 FILES_${PN}-dbg =+ "${libdir}/xtables/.debug"
+FILES_${PN}-doc =+ "${datadir}/xtables/"
 
 # The types patch is in the upstream folder, add it to the filespath
 FILESPATH =+ "${COREBASE}/meta/recipes-extended/${PN}/${PN}:"
@@ -34,9 +36,9 @@ SRC_URI[md5sum] = "b08a1195ec2c1ebeaf072db3c55fdf43"
 SRC_URI[sha256sum] = "77e6581f21f15946a814fa311236e5f3f7c6593180f9d695cea06aa95e464aba"
 
 
-CFLAGS += "-I${STAGING_KERNEL_DIR}/source/include/linux/netfilter_ipv4"
+CFLAGS += "-I${STAGING_KERNEL_BUILDDIR}/usr/include/linux/netfilter_ipv4"
 
-inherit autotools
+inherit autotools pkgconfig
 
 EXTRA_OECONF = "--with-kernel=${STAGING_INCDIR} \
                 ${@base_contains('DISTRO_FEATURES', 'ipv6', '', '--disable-ipv6', d)}"